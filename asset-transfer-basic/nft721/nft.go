1cdcc2ce857e3c9c696b6ca205b8e24872f49c19
fengxiaoxiao <13156050650@163.com>
2021-12-16 14:27:33 +0800

    不使用external service

--- asset-transfer-basic/nft721/nft.go
+++ asset-transfer-basic/nft721/nft.go
@@ -1,55 +1,51 @@
 package main
 
 import (
-	"github.com/hyperledger/fabric-chaincode-go/shim"
-	"log"
-	"os"
-
+	"fmt"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-
 	"nft721/nft"
 )
 
+func main() {
+	cc, err := contractapi.NewChaincode(new(nft.NFT721))
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := cc.Start(); err != nil {
+		fmt.Printf("Error starting NFT chaincode: %s", err)
+	}
+}
+
+//type serverConfig struct {
+//	CCID    string
+//	Address string
+//}
+//
 //func main() {
-//	cc, err := contractapi.NewChaincode(new(nft.NFT))
-//	if err != nil {
-//		panic(err.Error())
+//	// See chaincode.env.example
+//	config := serverConfig{
+//		CCID:    os.Getenv("CHAINCODE_ID"),
+//		Address: os.Getenv("CHAINCODE_SERVER_ADDRESS"),
 //	}
-//	if err := cc.Start(); err != nil {
-//		fmt.Printf("Error starting NFT chaincode: %s", err)
+//
+//	chaincode, err := contractapi.NewChaincode(new(nft.NFT721))
+//
+//	if err != nil {
+//		log.Panicf("error create nft7 chaincode: %s", err)
+//	}
+//
+//	server := &shim.ChaincodeServer{
+//		CCID:    config.CCID,
+//		Address: config.Address,
+//		CC:      chaincode,
+//		TLSProps: shim.TLSProperties{
+//			Disabled: true,
+//		},
+//	}
+//
+//	log.Println("success")
+//
+//	if err := server.Start(); err != nil {
+//		log.Panicf("error starting nft7 chaincode: %s", err)
 //	}
 //}
-
-type serverConfig struct {
-	CCID    string
-	Address string
-}
-
-func main() {
-	// See chaincode.env.example
-	config := serverConfig{
-		CCID:    os.Getenv("CHAINCODE_ID"),
-		Address: os.Getenv("CHAINCODE_SERVER_ADDRESS"),
-	}
-
-	chaincode, err := contractapi.NewChaincode(new(nft.NFT721))
-
-	if err != nil {
-		log.Panicf("error create nft7 chaincode: %s", err)
-	}
-
-	server := &shim.ChaincodeServer{
-		CCID:    config.CCID,
-		Address: config.Address,
-		CC:      chaincode,
-		TLSProps: shim.TLSProperties{
-			Disabled: true,
-		},
-	}
-
-	log.Println("success")
-
-	if err := server.Start(); err != nil {
-		log.Panicf("error starting nft7 chaincode: %s", err)
-	}
-}
